4a3dc196a865ceb63667372d28367eea2b11dbe5
julopesrocha <jujulrocha@gmail.com>
2022-05-07 21:31:24 -0300

    metodo newton-raphson

--- falsa-posicao.py
+++ falsa-posicao.py
@@ -5,14 +5,16 @@ def f(x):
     return y
 
 # vantagem em relação ao método da bissecao pois privilegia o menor valor no intervalo entre a e b
+# diferença forma de atualização da raiz 
 def falsaPos(a, b, error): 
     err = math.fabs(b-a)/2
+    iteracoes = 0
 
     # Teorema de Bolzano(verif se há uma raiz no intervalo)]
     if f(a)*f(b) < 0:
         
         while(err > error):
-            
+            iteracoes+=1
             medio = (a*f(b) - b*f(a)) / (f(b) - f(a)) 
 
             if f(medio) == 0:
@@ -24,11 +26,11 @@ def falsaPos(a, b, error):
                 else:
                     a = medio 
         if err <= error:
-            print(f'A raiz aproximada da função f será {medio}. Com {err}<={error}')
-        print(f'A raiz aproximada da função f será {medio}. Com erro: {err} >= {error}')
+            print(f'A raiz aproximada da função f será {medio} com f(x) = {f(medio)}. Com {err}<={error}')
+        print(f'A raiz aproximada da função f será {medio} com f(x) = {f(medio)}. Com erro: {err} >= {error}')
     else:
         print("Não há raiz nesse intervalo") 
-
+    print(f'Número de iteraçoẽs: {iteracoes}')
 
 #definir um intervalo [a,b] e um erro e
 falsaPos(2, 3, math.pow(10,-7))
